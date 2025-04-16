a9ff1bfe77dcc51ae7f948c7040aa46b2471532f
Ana Karolina Ferraro <ferraroanakarolina@gmail.com>
2025-04-16 12:53:15 -0300

    Add files via upload

---
+++ ex5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int recebeNumero(void);
+
+int soma(int x, int y); 
+
+void imprimeResultado (int resultado);
+
+
+int main(void) {
+        int n1, n2, resultado;
+
+        n1 = recebeNumero ();
+        printf ("Digitado: %d\n", n1);
+
+        n2 = recebeNumero();
+        printf ("Digitado: %d\n", n2);
+
+        resultado = soma(n1,n2);
+       imprimeResultado(resultado);
+       
+    return 0;
+}
+
+int recebeNumero(void) {
+    int x;
+
+    printf ("Digite um número: ");
+    scanf("%d", &x);
+
+
+return x;
+}
+
+int soma(int x, int y) {
+    return x + y;
+}
+    void imprimeResultado (int resultado) {
+    printf("Resultado: %d\n", resultado);
+    }
